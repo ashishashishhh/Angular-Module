432ae8ad1f21717f00810a81cc8ce81350d0e14c
ashishashishh <ashishalok12345@gmail.com>
2022-02-03 08:50:26 +0530

    Initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { DataService } from './data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  
+  displayedColumns = ['name','username','email'];
+  dataSource!:MatTableDataSource<any>;
+
+  @ViewChild('paginator') paginator : MatPaginator; 
+  @ViewChild(MatSort) matSort : MatSort;
+
+  constructor(private service: DataService) {}
+
+  ngOnInit() {
+      this.service.getUserData().subscribe((response:any) =>{
+      this.dataSource = new MatTableDataSource(response);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.matSort;
+    })
+  }
+
+  filterData($event : any){
+    this.dataSource.filter = $event.target.value;
+  }
+
+}
